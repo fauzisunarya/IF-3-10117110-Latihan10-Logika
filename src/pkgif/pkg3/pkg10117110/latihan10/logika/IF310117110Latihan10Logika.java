ddc27c704dad6f36a6b482f5ffb9741bd3a3eec9
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:11:19 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan10/logika/IF310117110Latihan10Logika.java
@@ -0,0 +1,23 @@
+
+package pkgif.pkg3.pkg10117110.latihan10.logika;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Umur menggunakan Tipe data boolean
+ */
+public class IF310117110Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+
+    }
+    
+}
